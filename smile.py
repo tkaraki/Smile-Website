6e86827fe37b8725dc9615490b76b6327cb8ad88
Tala Karaki <47926738+tkaraki@users.noreply.github.com>
2020-09-16 15:56:05 -0700

    Add files via upload

---
+++ smile.py
@@ -0,0 +1,9 @@
+from app import app,db
+from app.models import Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Post': Post}
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
